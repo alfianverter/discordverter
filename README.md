5bc1734e1433dd6237899d1f410e6cb9d14e54fd
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-11 21:34:06 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# discordverter
+## Tritax AI
+
+[![Build Status](https://travis-ci.org/travis-ci/travis-web.svg?branch=master)](https://travis-ci.org/travis-ci/travis-web) ![Version](https://img.shields.io/badge/version-v1.1.0-brightgreen.svg) <a href="https://discord.gg/HjJCwm5">
+        <img src="https://img.shields.io/discord/421853697027473408.svg?logo=discord"
+            alt="chat on Discord"></a>
+---
+            
+>**Tritax AI** is an **easy-to-use** fun Discord Bot with **HD quality music system** and very useful moderation commands.<br>
+
+***Want to provide feedback or request a feature?*** Come ask in my [Discord](https://discord.gg/hmVjJ).
+
+## Hosting
+
+Tritax AI is 24/7 hosted on glitch.<br>
+For self-hosting, it also comes with all the code that you need to set it up on glitch.
+
+
+## Features
+- Economy
+- Leveling
+- Coins
+- Utility
+- Music
+- Anime
+- Moderation
+- Fun
+- Memes
+- Fortnite
+- CS:GO
+
+## Setup
+
+Api Keys- For the anime, music, and imgur commands to work you have to get an api key.<br>
+For logging, please create two channels- `member-log`, `mod-log` for the bot to log moderation history, member join/leave history.
+
+Instructions:
+1. Clone the repository in a folder by doing `git clone <repo link>` in the command line. You can get the repo link from here<br>
+![screenshot_1](https://user-images.githubusercontent.com/37131433/38769030-d8cc16ba-4019-11e8-994f-9df5728a0f01.png)<br>
+* You can also use Github Desktop to clone it or download the `.zip` folder.<br>
+2. Change the prefix in the botconfig.json file to *whatever* you want.<br>
+3. Do `npm install` to install all the modules in the `package.json` file.<br>
+4. For hosting on just make a profile on glitch and grant it access o your github repo.<br>
+5. After that just import it to glitch and it will work.
+
+
+## Credits
+𝕽𝐞𝖝𝐨𝐢𝐋#4945 For general support and ideas and error help.<br>
+Respire#9313 For new ideas and Anime and Music commands help.<br>
+c4m3r0n#6175 For general support and ideas.<br>
+Bolt#6938  For general support and ideas.<br>
+
+
+## Support
+If any problem or error, feel free to ask in my Discord server or **open up an issue**.<br>
+Website: [Tritax AI](https://tritaxxcoder.github.io/TritaxAI)<br>
+Invite link is available on the website.<br>
+&copy; A Coder's Hangout 2018
---
+++ bot.js
@@ -0,0 +1,198 @@
+	const Discord = require("discord.js");
+	const botconfig = require("./botconfig.json");
+	const fs = require("fs");
+	let bot = new Discord.Client();
+	bot.commands = new Discord.Collection();
+	const coins = require("./coins.json");
+	const xp = require("./xp.json");
+	const db = require('quick.db');
+        const DBL = require("dblapi.js");
+        const dbl = new DBL(process.env.DBL_TOKEN, bot);
+
+
+	bot.on('ready', () => {
+	console.log("Loading...");
+	setTimeout(function(){
+	console.log("Bot has been loaded completely.");
+	}, 1000);
+	function botStatus() {
+        let status = [
+            `Verter lagi oprasi aku :"D.`,
+            `Aku lagi di anuin Verter >:V.`,
+            `Susah punya owner ganteng :D.`,
+            `Si verter terlalu ganteng :V`,
+            `Verter jangan bunuh aku.`
+        ];
+        let rstatus = Math.floor(Math.random() * status.length);
+
+        bot.user.setActivity(status[rstatus], {Type: 'STREAMING'});        // BOT STATUS
+      }; setInterval(botStatus, 20000)
+        setInterval(() => {
+        dbl.postStats(bot.guilds.size)
+        }, 1800000);
+	})
+
+	fs.readdir("./commands/", (err, files) => {
+    console.log(`Loaded ${files.length} commands.`)
+	if(err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js");
+	if(jsfile.length <= 0){
+	console.log("Couldn't find commands.");
+	return;
+	}
+
+
+	jsfile.forEach((f, i) =>{
+	let props = require(`./commands/${f}`);
+	console.log(`${f} loaded!`);
+	bot.commands.set(props.help.name, props);
+	});
+	});
+
+	bot.on("message", async message => {
+      let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+	
+    let prefix = prefixes[message.guild.id].prefixes;
+	if(message.author.bot) return undefined;
+	if(message.channel.type === 'dm') return ;
+        if(message.content.toLowerCase() === '<@421925809532436481>'){
+        let embed = new Discord.RichEmbed()
+       .setTitle("Tritax AI")
+       .addField("Prefix", `\`${prefix}\``, true)
+       .addField("Help", `\`${prefix}help\``, true)
+       .setThumbnail(bot.user.displayAvatarURL)
+       .setColor(`${message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : 0xffffff}`);
+        message.channel.send(embed);
+        }
+
+	let args = message.content.slice(prefix.length).trim().split(" ");
+	let cmd = args.shift().toLowerCase();
+	if(message.author.bot) return undefined;
+	if(!message.content.startsWith(prefix)) return undefined;
+   message.prefix = prefix;
+
+
+	try {
+	let commandFile = require(`./commands/${cmd}.js`);
+	commandFile.run(bot, message, args);
+	if(!commandFile) return message.channel.send("No command found with that name.");
+	} catch (e) { console.log(e) }
+
+	if(!coins[message.author.id]){
+	coins[message.author.id] = {
+	coins: 0
+	};
+	}
+
+	let coinAmt = Math.floor(Math.random() * 15) + 14;
+	let baseAmt = Math.floor(Math.random() * 15) + 14;
+ 
+
+	
+
+	if(coinAmt === baseAmt){
+	coins[message.author.id] = {
+	coins: coins[message.author.id].coins + coinAmt
+	};
+	fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+	if (err) console.log(err)
+	});
+
+	}
+
+	let xpAdd = Math.floor(Math.random() * 15) + 14;
+	
+
+	if(!xp[message.author.id]){
+	xp[message.author.id] = {
+	xp: 0,
+	level: 1
+	};
+	}
+
+
+	let curxp = xp[message.author.id].xp;
+	let curlvl = xp[message.author.id].level;
+	let nxtLvl = xp[message.author.id].level * 300;
+	xp[message.author.id].xp =  curxp + xpAdd;
+	if(nxtLvl <= xp[message.author.id].xp){
+	xp[message.author.id].level = curlvl + 1;
+
+	}
+	fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+	if(err) console.log(err)
+	});
+
+
+	});
+	bot.on('guildMemberAdd', member => {
+    bot.channels.get('454775818825236488').setName(`Total Users 〉 ${member.guild.memberCount}`)
+    let humans = member.guild.members.filter(m => !m.user.bot).size;
+    bot.channels.get('454775914069491722').setName(`Human 〉 ${humans}`)
+    let bots = member.guild.members.filter(m => m.user.bot).size;
+    bot.channels.get('454775966221336596').setName(`Robot 〉 ${bots}`)
+	const members = member.guild.memberCount;
+	const channel = member.guild.channels.find('name', 'member-log');
+	if (!channel) return;
+	
+       let Role = member.guild.roles.find(`name`, "Bots");
+       if(member.user.bot){
+	member.addRole(Role.id)
+       }else{
+      let role = member.guild.roles.find(`name`, "User");
+	member.addRole(role.id)
+       }
+ 
+	let Embed = new Discord.RichEmbed()
+	.setFooter(`User Joined | Members- ${member.guild.memberCount}`)
+	.setColor("#cde246")    
+	.setAuthor(`${member.displayName} has joined ${member.guild.name}`, member.user.displayAvatarURL)
+	.setTimestamp()
+	channel.send(Embed);
+	});
+	bot.on('guildMemberRemove', member => {
+    bot.channels.get('454775818825236488').setName(`Total Users 〉 ${member.guild.memberCount}`)
+    let humans = member.guild.members.filter(m => !m.user.bot).size;
+    bot.channels.get('454775914069491722').setName(`Human 〉 ${humans}`)
+    let bots = member.guild.members.filter(m => m.user.bot).size;
+    bot.channels.get('454775966221336596').setName(`Robot 〉 ${bots}`)
+	const channel = member.guild.channels.find(`name`, 'member-log');
+	if(!channel) return; 
+	let Embed = new Discord.RichEmbed()
+	.setColor("#e26346")
+	.setAuthor(`${member.displayName}, has left ${member.guild.name}.`, member.user.displayAvatarURL)
+	.setTimestamp()
+	.setFooter(`User Left | Members- ${member.guild.memberCount}`)
+	channel.send(Embed);
+	});
+
+	bot.on('guildCreate', guild => {
+	      let channel = bot.channels.get("428564028239904790")
+        const embed = new Discord.RichEmbed()
+        .setColor("#cde246")
+        .setAuthor(`Joined ${guild.name}`)
+        .setThumbnail(guild.iconURL)
+        .addField("Owner", guild.owner.user.tag)
+        .addField("ID", guild.id, true)
+        .addField("Users", guild.memberCount, true)
+        .addField("Channels", guild.channels.size, true)
+         channel.send(embed);
+	});
+	bot.on('guildDelete', guild => {
+	      let channel = bot.channels.get("428564028239904790")
+        const embed = new Discord.RichEmbed()
+        .setColor("#cde246")
+        .setAuthor(`Left ${guild.name}`)
+        .setThumbnail(guild.iconURL)
+        .addField("Owner", guild.owner.user.tag)
+        .addField("ID", guild.id, true)
+        .addField("Users", guild.memberCount, true)
+        .addField("Channels", guild.channels.size, true)
+         channel.send(embed);
+	});
+	bot.login(process.env.TOKEN);
