7d4d378d543f15d65a267e86e4b5704b49b7b032
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 05:56:37 +0700

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -2,23 +2,29 @@ const Discord = require("discord.js");
 let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-  //!coins
+ 
   if(!coins[message.author.id]){
     coins[message.author.id] = {
       coins: 0
     };
   }
 
-  let uCoins = coins[message.author.id].coins;
+  let userCoins = coins[message.author.id].coins;
 
-
+  let coinicon = message.author.displayAvatarURL
+  if(message.author.id !== '315524485501550594') return message.channel.send ("<:toohappy:443956536486789131> As this command is still in development, It is limited to FrostedWeFall#8609 Only");
   let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#00FF00")
-  .addField("💸", uCoins);
+  .setAuthor(`${message.author.username}'s Balance`)
+  .setDescription(`Here is ${message.author.username}'s Balance!`)
+  .setColor("#0263ff")
+  .setThumbnail(coinicon)
+  .addField( "Coins 📀", `${userCoins} \\📀`, true)
+  .addField("Gain more coins", "by talking more in chat!", true)
+  .addField("Want to send coins to people?", "~pay <user> <amount>", true)
+  .setFooter(`Requested By ${message.author.username} ID: ${message.author.id}`, message.author.displayAvatarURL);
+  const mess = await message.channel.send(`<a:customloading:439644282828226571> Loading Coins of user ${message.author.id}`);
+  mess.edit(coinEmbed)
 
-    message.channel.send(":white_check_mark: | **A private message has been send!**")
-  message.author.send(coinEmbed).then(message =>{message.react("🤑")});
 }
 
 module.exports.help = {
