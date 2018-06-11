02a3fdea097ac58490c3cc243fa8a825d9f3d16c
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 06:13:06 +0700

    Create pay.js

---
+++ commands/pay.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let Coins = require("../coins.json");
+
+module.exports.run = async (bot, message, args) => {
+
+
+  if(!Coins[message.author.id]){
+    return message.reply("You don't have any Coins! Earn some by talking in chat! :)")
+  }
+
+  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+
+  if(!Coins[pUser.id]){
+    Coins[pUser.id] = {
+      Coins: 0
+    };
+  }
+
+  let pCoins = Coins[pUser.id].Coins;
+  let sCoins = Coins[message.author.id].Coins;
+
+  if(sCoins < args[0]) return message.reply("Not enough Coins there!");
+
+  Coins[message.author.id] = {
+    Coins: sCoins - parseInt(args[1])
+  };
+
+  Coins[pUser.id] = {
+    Coins: pCoins + parseInt(args[1])
+  };
+  let pay = new Discord.RichEmbed()
+    .setTitle("coin Transactions")
+    .setColor("#0263ff")
+    .addField("Sender", message.author)
+    .addField("Receiver", pUser)
+    .addField("Coin Amount", `${args[1]}`)
+    .setFooter(`Requested By ${message.author.username} ID: ${message.author.id}`, message.author.displayAvatarURL);
+
+    message.channel.send(pay).then(msg => {msg.delete(25000)});
+    console.log(`${message.author} has sent ${pUser} ${args[1]} Coins. :ring:`);
+
+  fs.writeFile("./coins.json", JSON.stringify(Coins), (err) => {
+    if(err) console.log(err)
+  });
+
+
+}
+
+module.exports.help = {
+  name: "pay"
+}
