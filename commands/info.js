22c6e9c473a871fed75f595805aad9c45bcdab5b
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 06:18:41 +0700

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,52 @@
-const Discord = require('discord.js');
-module.exports.run = async (bot, message, args) => {
+const Discord = require("discord.js");
+var upSecs = 0;
+var upMins = 0;
+var upHours = 0;
+var upDays = 0;
+//(◕ ◡ ◕)
 
-    let embed = new Discord.RichEmbed()
- 
-   .setDescription("[Upvote my bot](https://discordbots.org/bot/434304339268337665/vote)\n[Join my server](https://discord.gg/b9pkqk6)\n\nHelp Command- `>help`")
-   .addField("OWNER", "<@394498595065298945>")
-   message.channel.send(embed);
-}
-module.exports.help = {
-    name: "info"
+	setInterval(function() {
+		upSecs = upSecs + 1
+		if (upSecs >= 60) {
+			upSecs = 0
+			upMins = upMins + 1
+		}
+		if (upMins >= 60) {
+			upMins = 0
+			upHours = upHours + 1
+		}
+		if (upHours >= 24) {
+			upHours = 0
+			upDays = upDays + 1
+
+		}
+
+
+	}, 1000)
+
+module.exports.run = async (bot, message, args) => {
+    const botinfo = [
+            '__Information About **Frost**__ ❄',
+            '',
+            'Frost is a **multi-purpose** discord bot with exciting features!',
+            '',
+            `» I'm currently in **${bot.guilds.size}** guilds.`,
+            `» I'm checking **${bot.channels.size}** channels.`,
+            `» I'm watching over **${bot.users.size}** other users.`,
+            `» I'm running Node.js version **${process.version}**.`,
+            `» I'm running Discord.js version **${require('discord.js').version}.**`,
+            `» I'm using **${~~(process.memoryUsage().heapUsed / 1024 / 1024)}mb** of RAM`,
+            `» I've been online for **${upDays}** Days **${upHours}** Hours **${upMins}** Minutes **${upSecs}** Seconds!`,
+            `Be sure to invite Frost by typing **~invite**!`
+        ].join('\n');
+        message.channel.send(botinfo);
+   
+
+
+   }
+  
+  
+  
+  module.exports.help = {
+  name: "info"
 }
