fdc8618d4a756fcf0a10688155984efd60bcff77
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 06:04:24 +0700

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -8,7 +8,32 @@ module.exports.run = async (bot, message, args) => {
       coins: 0
     };
   }
-
+  if(message.author.bot) return;
+  if(message.channel.type !== "text") return;
+  
+  let members = [];
+  let indexes = [];
+  
+  message.guild.members.forEach(function(member){
+    members.push(member.user.username);
+    indexes.push(member.id);
+  })
+  
+  let match = sm.findBestMatch(args.join(' '), members);
+  let username = match.bestMatch.target;
+  
+    let member = message.guild.members.get(indexes[members.indexOf(username)])
+    
+     let definedUser = "";
+     let definedUser2 = "";
+    if(!args[0]) {
+      definedUser = message.author
+      definedUser2 = message.member
+    } else {
+      let mention = message.mentions.users.first()
+      definedUser = mention || member.user
+        definedUser2 = message.mentions.members.first() || message.guild.members.get(args[0]) || member
+    }
   let userCoins = coins[message.author.id].coins;
 
   let coinicon = message.author.displayAvatarURL
