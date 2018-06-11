43ecd7093adeea5e809481ad457c6333a855dedc
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 06:17:21 +0700

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -2,27 +2,27 @@ const Discord = require("discord.js");
 let coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-
-  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]); 
-
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
+ 
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
       coins: 0
     };
   }
-  let userCoins = coins[pUser.id].coins;
 
-  let coinicon = pUser.displayAvatarURL
+  let userCoins = coins[message.author.id].coins;
+
+  let coinicon = message.author.displayAvatarURL
+  if(message.author.id !== '315524485501550594') return message.channel.send ("<:toohappy:443956536486789131> As this command is still in development, It is limited to FrostedWeFall#8609 Only");
   let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(`${pUser.username}'s Balance`)
-  .setDescription(`Here is ${pUser.username}'s Balance!`)
+  .setAuthor(`${message.author.username}'s Balance`)
+  .setDescription(`Here is ${message.author.username}'s Balance!`)
   .setColor("#0263ff")
   .setThumbnail(coinicon)
   .addField( "Coins 📀", `${userCoins} \\🔘`, true)
   .addField("Gain more coins", "by talking more in chat!", true)
-  .addField("Want to send coins to people?", ">pay <user> <amount>", true)
-  .setFooter(`Requested By ${pUser.username} ID: ${pUser.id}`, pUser.displayAvatarURL);
-  const mess = await message.channel.send(`🔄 Loading Coins of user ${pUser.id}`);
+  .addField("Want to send coins to people?", "~pay <user> <amount>", true)
+  .setFooter(`Requested By ${message.author.username} ID: ${message.author.id}`, message.author.displayAvatarURL);
+  const mess = await message.channel.send(`🔄 Loading Coins of user ${message.author.id}`);
   mess.edit(coinEmbed)
 
 }
