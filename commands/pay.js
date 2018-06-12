c6e70789ec477706876b97ea57fb4f2117bb9cd2
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-12 12:19:13 +0700

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -1,39 +1,48 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let coins = require("../coins.json");
+let Coins = require("../coins.json");
 
 module.exports.run = async (bot, message, args) => {
-  //!pay @isatisfied 59345
 
-  if(!coins[message.author.id]){
-    return message.reply("You don't have any coins!")
+
+  if(!Coins[message.author.id]){
+    return message.reply("You don't have any Coins! Earn some by talking in chat! :)")
   }
 
   let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
-      coins: 0
+  if(!Coins[pUser.id]){
+    Coins[pUser.id] = {
+      Coins: 0
     };
   }
 
-  let pCoins = coins[pUser.id].coins;
-  let sCoins = coins[message.author.id].coins;
+  let pCoins = Coins[pUser.id].Coins;
+  let sCoins = Coins[message.author.id].Coins;
 
-  if(sCoins < args[0]) return message.reply("Not enough coins there!");
+  if(sCoins < args[0]) return message.reply("Not enough Coins there!");
 
-  coins[message.author.id] = {
-    coins: sCoins - parseInt(args[1])
+  Coins[message.author.id] = {
+    Coins: sCoins - parseInt(args[1])
   };
 
-  coins[pUser.id] = {
-    coins: pCoins + parseInt(args[1])
+  Coins[pUser.id] = {
+    Coins: pCoins + parseInt(args[1])
   };
+  if(message.author.id !== '315524485501550594') return message.channel.send ("<:toohappy:443956536486789131> As this command is still in development, It is limited to FrostedWeFall#8609 Only");
+  let pay = new Discord.RichEmbed()
+    .setTitle("coin Transactions")
+    .setColor("#0263ff")
+    .addField("Sender", message.author)
+    .addField("Receiver", pUser)
+    .addField("Coin Amount", `${args[1]}`)
+    .setFooter(`Requested By ${message.author.username} ID: ${message.author.id}`, message.author.displayAvatarURL);
 
-  message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
+    message.channel.send(pay).then(msg => {msg.delete(25000)});
+    console.log(`${message.author} has sent ${pUser} ${args[1]} Coins. :ring:`);
 
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if(err) cosole.log(err)
+  fs.writeFile("./coins.json", JSON.stringify(Coins), (err) => {
+    if(err) console.log(err)
   });
 
 
